a2f2353f0f116d6dc8403626cd0798be48102d85
fit-s-u-m <nehemiahmaths@gmail.com>
2025-12-30 11:26:33 +0300

    fix header should be byte not string in websocket

--- api/websocket.py
+++ api/websocket.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from typing import List
-# Manage connected clients
+
 class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
-        await websocket.accept()
+        await websocket.accept(
+            headers=[(b"access-control-allow-origin", b"*")]
+        )
         self.active_connections.append(websocket)
 
     def disconnect(self, websocket: WebSocket):
@@ -16,21 +18,19 @@ class ConnectionManager:
         for connection in self.active_connections:
             await connection.send_json(message)
 
+
 manager = ConnectionManager()
 
 def connect_websocket_app(app: FastAPI):
 
     @app.websocket("/ws/transactions")
     async def websocket_endpoint(websocket: WebSocket):
-        await websocket.accept(headers=[("Access-Control-Allow-Origin", "*")])
         await manager.connect(websocket)
         try:
             while True:
-                data = await websocket.receive_json()  # Receive messages from client
+                data = await websocket.receive_json()
                 print(f"Received: {data}")
-                # Optionally, broadcast it to all other clients
                 await manager.broadcast(data)
         except WebSocketDisconnect:
             manager.disconnect(websocket)
             print("Client disconnected")
-
